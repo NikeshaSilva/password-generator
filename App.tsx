210e350205123e22f211518a177e2a2d7837973f
Nikesha Silva <nikesha.silva@innovative-e.com>
2025-07-07 23:05:02 +0530

    Added yup validation

--- App.tsx
+++ App.tsx
@@ -1,28 +1,37 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
+import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+//form validation with yup
+import * as yup from 'yup';
 
-import { NewAppScreen } from '@react-native/new-app-screen';
-import { StatusBar, StyleSheet, useColorScheme, View } from 'react-native';
+import { useState } from 'react';
 
-function App() {
-  const isDarkMode = useColorScheme() === 'dark';
+export default function App() {
+  const [password, setPassword] = useState<string>('');
+  const [isPwGenerated, setIsPwGenerated] = useState<boolean>(false);
+  const [lowercase, setLowercase] = useState<boolean>(true);
+  const [uppercase, setUppercase] = useState<boolean>(false);
+  const [numbers, useNumbers] = useState<boolean>(false);
+  const [symbols, useSymbols] = useState<boolean>(false);
+
+  const generatePasswordString = (passwordLength: number) => {};
+
+  const createPassword = (characters: string, passwordLength: number) => {};
+
+  const resetPasswordState = () => {};
+
+  const PasswordSchema = yup.object().shape({
+    passwordLength: yup
+      .number()
+      .min(4, 'Should be minimum of 4 characters.')
+      .max(16, 'Should be maximum of 16 characters')
+      .required(),
+  });
 
   return (
-    <View style={styles.container}>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <NewAppScreen templateFileName="App.tsx" />
+    <View>
+      <Text>App</Text>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-});
-
-export default App;
+const styles = StyleSheet.create({});
